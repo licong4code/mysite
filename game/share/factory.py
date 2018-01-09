b90c3f4270abcbbe65b0e57e00324ef6d9fa88ef
licong <licong28@126.com>
2018-01-09 17:39:48 +0800

    新增uswgi配置

---
+++ game/share/factory.py
@@ -0,0 +1,39 @@
+# coding:utf-8
+from PIL import Image,ImageDraw,ImageFont
+import time,os
+import datetime
+
+base_root = os.path.join(os.path.split(__file__)[0],"../../download/")
+
+class Creator(object):
+    def __init__(self,bgpath,fontpath,fontsize):
+        self.ttffont = ImageFont.truetype(fontpath,fontsize)
+        self.im = Image.open(bgpath)
+        self.draw = ImageDraw.Draw(self.im)
+
+    def addText(self,text,color,position):
+        self.draw.text(position, text, fill = color, font=self.ttffont)
+
+    def save(self,outpath):
+        self.im.save(outpath)
+
+def run(data):
+    root = os.path.join(base_root,"share")
+    now = datetime.datetime.now()
+    foleder = os.path.join(base_root, now.strftime('%Y-%m-%d'))
+    if os.path.exists(foleder) == False:
+        os.mkdir(foleder)
+    filename = "{0}/{1}.jpg".format(foleder, int(time.time()))
+
+    ctor = Creator(os.path.join(root,"bg.jpg"),os.path.join(root,"msyhbd.ttf"),36)
+    x,y = 180,580
+    for value in data:
+        ctor.addText(value["text"],value["color"],(x,y))
+        x = x + ctor.ttffont.getsize(value["text"])[0]
+    ctor.save(os.path.join(root, filename))
+    return filename.replace(base_root,"")
+
+if __name__ == "__main__":
+	# run([{"text":u"累计淘汰 1 名 获得 ","color":(18,10,10)},{"text":u"1.88","color":(255,0,0)},{"text":u' 微信红包',"color":(18,10,10)}])
+	run([{"text":u" 获得 ","color":(18,10,10)},{"text":u"1.88","color":(255,0,0)},{"text":u' 微信红包',"color":(18,10,10)}])
+	# run([{"text":u"1.88","color":(255,0,0)},{"text":u' 微信红包',"color":(18,10,10)}])
---
+++ game/views.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.db import transaction
+from django.http import JsonResponse
+
+from django.shortcuts import render
+from share import factory
+# Create your views here.
+
+#
+def share(request):
+    try:
+        filename = factory.run([{"text":u" 获得 ","color":(18,10,10)},{"text":u"1.88","color":(255,0,0)},{"text":u' 微信红包',"color":(18,10,10)}])
+        url = "http://update.pukegame.com/download/" + filename
+        return JsonResponse({"code": 0,"msg":url})
+    except Exception, e:
+        return JsonResponse({"code": - 1,"msg":str(e)})
