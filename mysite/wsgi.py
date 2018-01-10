9aefa1c3d7fffa0170629bea5d9dcae4be6bd9bf
licong <licong28@126.com>
2018-01-10 18:37:18 +0800

    新增uswgi配置

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,7 +10,5 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 application = get_wsgi_application()
