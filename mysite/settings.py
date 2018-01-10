12a8dd869d18b00a3b7cfc12da27d17820a06241
licong <licong28@126.com>
2018-01-10 18:53:30 +0800

    新增uswgi配置

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,8 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT='/home/gamecenter/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT='/home/gamecenter/static'
