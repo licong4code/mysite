d599e330537eb8540ce7cd68feedfe3e954e03a1
licong <licong28@126.com>
2018-01-12 14:36:14 +0800

    完善图片合成

--- game/share/factory.py
+++ game/share/factory.py
@@ -1,39 +1,111 @@
 # coding:utf-8
 from PIL import Image,ImageDraw,ImageFont
-import time,os
-import datetime
+import time,os,io,datetime,hashlib,json
+from urllib2 import urlopen
 
-base_root = os.path.realpath(os.path.join(os.path.split(__file__)[0],"../../download/"))
+base_root = (os.path.realpath(os.path.join(os.path.split(__file__)[0],"../../")) + "/download/").replace("\\","/")
+def md5(src):
+	m2 = hashlib.md5()
+	m2.update(src)
+	return m2.hexdigest()
 
 class Creator(object):
-    def __init__(self,bgpath,fontpath,fontsize):
-        self.ttffont = ImageFont.truetype(fontpath,fontsize)
-        self.im = Image.open(bgpath)
-        self.draw = ImageDraw.Draw(self.im)
+    img_cache_icons = {}
+    img_cache_logs = {}
+    img_cache_qrs = {}
+    img_cache_bgs = {}
+    def __init__(self):
+        self.ttffont = ImageFont.truetype("{0}/share/msyhbd.ttf".format(base_root),36)
+        self.head_path = os.path.join(base_root,"head")
+        if os.path.exists(self.head_path) == False:
+        	os.mkdir(self.head_path)
+
+    def getBg(self,type):
+        if self.img_cache_bgs.has_key(type) == False:
+            img = Image.open("{0}/share/bg{1}.jpg".format(base_root,type))
+            self.img_cache_bgs[type] = img
+
+        return self.img_cache_bgs[type].copy()
+
+    def getIcon(self,app_id):
+        if self.img_cache_icons.has_key(app_id) == False:
+            img = Image.open("{0}/share/icon{1}.png".format(base_root,app_id))
+            self.img_cache_icons[app_id] = img
+        return self.img_cache_icons[app_id]
+    
+    def downloadIcon(self,url): 
+
+    	outpath = os.path.join(self.head_path,md5(url) + ".jpg")
+    	img = None
+    	if os.path.exists(outpath) == False:
+	    	try:
+	    		bytes = io.BytesIO(urlopen(url).read())
+	    		img = Image.open(bytes)
+	    		img.save(outpath)
+	    		return img
+	    	except Exception as e:
+	    		print e
+    	else:
+    		return Image.open(outpath)
+
+    	return None
+
+
+    def getLogo(self,app_id):
+        if self.img_cache_logs.has_key(app_id) == False:
+            img = Image.open("{0}/share/logo{1}.png".format(base_root,app_id))
+            self.img_cache_logs[app_id] = img
+        return self.img_cache_logs[app_id]
+
+    def getQR(self,app_id):
+        if self.img_cache_qrs.has_key(app_id) == False:
+            img = Image.open("{0}/share/qr{1}.png".format(base_root,app_id))
+            self.img_cache_qrs[app_id] = img
+        return self.img_cache_qrs[app_id]
 
     def addText(self,text,color,position):
         self.draw.text(position, text, fill = color, font=self.ttffont)
 
-    def save(self,outpath):
-        self.im.save(outpath)
+    def __dobuild(self,data):
+    	filename = md5(json.dumps(data))
+        now = datetime.datetime.now()
+    	foleder = os.path.realpath(os.path.join(base_root, now.strftime('%Y-%m-%d')))
+    	if os.path.exists(foleder) == False:
+        	os.mkdir(foleder)
+       	filename = "{0}/{1}.jpg".format(foleder, filename).replace("\\","/")
+       	if os.path.exists(filename) == False:
+	        bg = self.getBg(data["type"])
+	        if data.has_key("icon"):
+	        	icon = self.downloadIcon(data['icon'])
+	        else:
+	        	icon = self.getIcon(data["app_id"])
+	        logo = self.getLogo(data["app_id"])
+	        qr = self.getQR(data["app_id"])
+	        draw=ImageDraw.Draw(bg)
+	        bg.paste(qr,(bg.size[0]/2 - qr.size[0]/2,bg.size[1] - qr.size[1] - 10))
+	        r,g,b,a = logo.split()
+	        bg.paste(logo,(10,10),mask = a)
+	        bg.paste(icon,(112 - icon.size[0]/2,384 -  icon.size[1]/2))
+			
+	        for v in data['data']:
+	        	x,y = v['position'][0] - self.ttffont.getsize(v["text"])[0]/2,v['position'][1]
+	        	draw.text((x,y),v['text'],font = self.ttffont,fill = v['color'])
 
-def run(data):
-    root = os.path.join(base_root,"share")
-    now = datetime.datetime.now()
-    foleder = os.path.realpath(os.path.join(base_root, now.strftime('%Y-%m-%d')))
-    if os.path.exists(foleder) == False:
-        os.mkdir(foleder)
-    filename = "{0}/{1}.jpg".format(foleder, int(time.time()))
+	        bg.save(os.path.join(base_root,filename))
+		
+        return filename.replace(base_root,"")
 
-    ctor = Creator(os.path.join(root,"bg.jpg"),os.path.join(root,"msyhbd.ttf"),36)
-    x,y = 180,580
-    for value in data:
-        ctor.addText(value["text"],value["color"],(x,y))
-        x = x + ctor.ttffont.getsize(value["text"])[0]
-    ctor.save(os.path.join(root, filename))
-    return filename.replace(base_root,"").replace("\\","/")
+    def build(self,data):
 
+    	if data['type'] == 1:
+    		return self.__dobuild({"type":data['type'],"app_id":data['app_id'],'icon':data['icon'],"data":[{"text":data['name'],"color":(0,0,0) , "position":(218,478)},{"text":str(data['total']),"color":(255,0,0) , "position":(336,560)}]})
+    	else:
+    		return self.__dobuild({"type":data['type'],"app_id":data['app_id'],'icon':data['icon'],"data":[{"text":data['name'],"color":(0,0,0) , "position":(192,478)},{"text":str(data['race']),"color":(255,0,0) , "position":(340,478)},{"text":str(data['rank']),"color":(255,0,0) , "position":(182,562)},{"text":str(data['red']),"color":(255,0,0) , "position":(432,562)}]})
 if __name__ == "__main__":
-	# run([{"text":u"累计淘汰 1 名 获得 ","color":(18,10,10)},{"text":u"1.88","color":(255,0,0)},{"text":u' 微信红包',"color":(18,10,10)}])
-	run([{"text":u" 获得 ","color":(18,10,10)},{"text":u"1.88","color":(255,0,0)},{"text":u' 微信红包',"color":(18,10,10)}])
-	# run([{"text":u"1.88","color":(255,0,0)},{"text":u' 微信红包',"color":(18,10,10)}])
+	ctor = Creator()
+	print ctor.build({"type":1,"app_id":1,"name":u'白金岛',"total":100,"icon":"http://wx.qlogo.cn/mmopen/vi_32/920MZaSOicrlLDvP5UmVp7uqxleZFIHZDdIFokkwBkYG1fHJe4rdNbpn8JHYvibzP5mXaH1Apa110r6TwTOKPndQ/132"})
+	# ctor.dobuild({"type":1,"app_id":2,"data":[{"text":u"licong2","color":(0,0,0) , "position":(218,478)},{"text":u"50 ","color":(255,0,0) , "position":(336,560)}]},"share/out1.jpg")
+	# ctor.dobuild({"type":2,"app_id":2,"data":[{"text":u"licong2","color":(0,0,0) , "position":(192,478)},{"text":u"100","color":(255,0,0) , "position":(340,478)},{"text":u"50","color":(255,0,0) , "position":(182,562)},{"text":u"50","color":(255,0,0) , "position":(432,562)}]},"share/out.jpg")
+	# ctor.downloadIcon("http://wx.qlogo.cn/mmopen/vi_32/920MZaSOicrlLDvP5UmVp7uqxleZFIHZDdIFokkwBkYG1fHJe4rdNbpn8JHYvibzP5mXaH1Apa110r6TwTOKPndQ/132")
+
+	print json.dumps({"type":1,"app_id":1,"name":u'白金岛',"total":100,"icon":"http://wx.qlogo.cn/mmopen/vi_32/920MZaSOicrlLDvP5UmVp7uqxleZFIHZDdIFokkwBkYG1fHJe4rdNbpn8JHYvibzP5mXaH1Apa110r6TwTOKPndQ/132"},ensure_ascii=False)
--- game/views.py
+++ game/views.py
@@ -6,16 +6,100 @@ from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.db import transaction
 from django.http import JsonResponse
+import MySQLdb
 
 from django.shortcuts import render
 from share import factory
-# Create your views here.
+import json,base64
+from Crypto.Cipher import AES
+from Crypto import Random
+from binascii import b2a_hex, a2b_hex
+KEY = 'UEK9B4QejJqCAYhw'
 
-#
+# Create your views here.
+share_img_factory = None
+red_db = None
+red_cursor = None
+
+from Crypto.Cipher import AES
+from binascii import b2a_hex, a2b_hex
+
+
+class prpcrypt():
+    def __init__(self, key):
+        self.key = key
+        self.mode = AES.MODE_CBC
+
+    # 加密函数，如果text不足16位就用空格补足为16位，
+    # 如果大于16当时不是16的倍数，那就补足为16的倍数。
+    def encrypt(self, text):
+        cryptor = AES.new(self.key, self.mode, b'0000000000000000')
+        # 这里密钥key 长度必须为16（AES-128）,
+        # 24（AES-192）,或者32 （AES-256）Bytes 长度
+        # 目前AES-128 足够目前使用
+        length = 16
+        count = len(text)
+        if count < length:
+            add = (length - count)
+            # \0 backspace
+            text = text + ('\0' * add)
+        elif count > length:
+            add = (length - (count % length))
+            text = text + ('\0' * add)
+        self.ciphertext = cryptor.encrypt(text)
+        # 因为AES加密时候得到的字符串不一定是ascii字符集的，输出到终端或者保存时候可能存在问题
+        # 所以这里统一把加密后的字符串转化为16进制字符串
+        return b2a_hex(self.ciphertext)
+
+    # 解密后，去掉补足的空格用strip() 去掉
+    def decrypt(self, text):
+        cryptor = AES.new(self.key, self.mode, b'0000000000000000')
+        plain_text = cryptor.decrypt(a2b_hex(text))
+        return plain_text.rstrip('\0')
+
+
+def decode_base64(data):
+    """Decode base64, padding being optional.
+
+    :param data: Base64 data as an ASCII byte string
+    :returns: The decoded byte string.
+
+    """
+    missing_padding = 4 - len(data) % 4
+    if missing_padding:
+        data += b'=' * missing_padding
+    return base64.decodestring(data)
+
+
 def share(request):
     try:
-        filename = factory.run([{"text":u" 获得 ","color":(18,10,10)},{"text":u"1.88","color":(255,0,0)},{"text":u' 微信红包',"color":(18,10,10)}])
-        url = "http://update.pukegame.com/download/" + filename
+        global share_img_factory
+        if share_img_factory == None:
+            share_img_factory = factory.Creator()
+        data = json.loads(decode_base64(request.GET["data"]))
+        if data["type"] == 1:
+            def conndb():
+                global red_db
+                global red_cursor
+                red_db = MySQLdb.connect(host="xxx.79.2.xxx", port=0, user='', passwd='', db='',charset='utf8')
+                red_cursor = red_db.cursor()
+
+            try:
+                if red_db == None:
+                    conndb()
+                else:
+                    red_db.ping()
+            except Exception as e:
+                conndb()
+
+            sql = 'SELECT amount FROM bjd_red_packet_tools WHERE account="{0}"'.format(data['account'])
+            result = red_cursor.execute(sql)
+            if result >  0:
+                data["total"] = red_cursor.fetchone()[0]
+
+        url = "http://update.pukegame.com/download/" + share_img_factory.build(data)
         return JsonResponse({"code": 0,"url":url})
+        # else:
+        #     return JsonResponse({"code": -1, "msg": u'非法请求'})
     except Exception, e:
         return JsonResponse({"code": - 1,"msg":str(e)})
